083fe519eae0442fd3dcf91fe959528f65dce38c
gal2004 <galpalas265@gmail.com>
2019-04-04 10:08:33 +0300

    #2

---
+++ MyProfile/app/src/main/java/com/example/myprofile/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.myprofile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn;
+    int counter = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn = (Button)findViewById(R.id.button1);
+
+    }
+
+    public void CountClick(View v)
+    {
+        counter++;
+        btn.setText("Likes number:" + String.valueOf(counter));
+    }
+}
