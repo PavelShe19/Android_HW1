e609e1a7e5f4b77c12439b880352a42ce38cb5ca
PavelShe19 <48412351+PavelShe19@users.noreply.github.com>
2019-04-07 15:08:21 +0300

    added _gal to names
    changed main file name with _gal suffix to indicate uniqueness.

--- MyProfile/app/src/main/java/com/example/myprofile/MainActivity_gal.java
+++ MyProfile/app/src/main/java/com/example/myprofile/MainActivity_gal.java
@@ -5,14 +5,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity_gal extends AppCompatActivity {
 
     Button btn;
     int counter = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_gal);
 
         btn = (Button)findViewById(R.id.button1);
 
